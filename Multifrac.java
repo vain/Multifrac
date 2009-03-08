b56ce9fe3a37dc129a9c42f2a1d7ccdd312bcfde
Peter Hofmann <scm@uninformativ.de>
2009-03-08 23:18:46 +0100

    So ne Anfangsidee mit Grundmandelbrot, aber Organisation nicht so toll - lieber eine Architektur nehmen, die ähnliche wie das C++-Multifrac arbeitet, also mit einem einzigen Buffer und "dirty"

---
+++ Multifrac.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Multifrac extends JFrame
+{
+	protected RenderPanel rend = null;
+
+	public Multifrac()
+	{
+		// RenderPanel
+		rend = new RenderPanel();
+		rend.setPreferredSize(new Dimension(512, 512));
+		rend.setVisible(true);
+		add(rend);
+
+		// Properties of the window itself
+		setTitle("Multifrac");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		pack();
+		setVisible(true);
+	}
+
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				new Multifrac();
+			}
+		});
+	}
+}
