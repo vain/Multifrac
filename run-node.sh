6c26d73f0e7ee8418418d6c7196c47d56bdc231f
Peter Hofmann <scm@uninformativ.de>
2009-08-08 11:32:21 +0200

    Run-scripts: Heap size can be set via argv[]

--- run-node.sh
+++ run-node.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-java -cp dist/Multifrac.jar multifrac.net.Node "$@"
+MEMSIZE=64m
+
+if [[ "$1" == "-m" ]]
+then
+	MEMSIZE="$2"
+	shift; shift
+fi
+
+set -x
+java -Xmx${MEMSIZE} -cp dist/Multifrac.jar multifrac.net.Node "$@"
--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-java -Xmx1800m -jar dist/Multifrac.jar
+MEMSIZE=1800m
+
+if [[ "$1" == "-m" ]]
+then
+	MEMSIZE="$2"
+	shift; shift
+fi
+
+set -x
+java -Xmx${MEMSIZE} -jar dist/Multifrac.jar "$@"
